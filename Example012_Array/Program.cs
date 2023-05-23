653264bb8af63788c7fab8ec19a1117e0a170718
Gridin_Andrei <andreigridin918@gmail.com>
2023-05-23 20:25:14 +0500

    Add Example 012

---
+++ Example012_Array/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+int[] array = {1, 9, 2, 8, 3, 4, 7, 5, 6, 10};
+int n = 0;
+
+int Array(int j)
+{
+    while(array[n] != j)
+    {
+        n++;
+    }
+    return n;
+}
+
+Console.Write("Введите число от 1 до 10: ");
+int i = int.Parse(Console.ReadLine()!);
+
+Console.Write($"Число {i} находится в array[{Array(i)}]");
