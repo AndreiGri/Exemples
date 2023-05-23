3a7e3c712ed9fb2550c769e281ce6926517c0e06
Gridin_Andrei <andreigridin918@gmail.com>
2023-05-23 20:23:33 +0500

    Add Example 011

---
+++ Example011_ArrayMethod/Program.cs
@@ -0,0 +1,14 @@
+﻿int Max(int a, int b, int c)
+{
+    int max = a;
+    if(b > max) max = b;
+    if(c > max) max = c;
+    return max;
+}
+int[] array={15, 72, 390, 74, 56, 69, 97, 18, 179};
+
+int max = Max(Max(array[0], array[1], array[2]),
+          Max(array[3], array[4], array[5]),
+          Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
