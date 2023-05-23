cc3ccb5e511ad9da7335fab09cc05247be8d0ff1
Gridin_Andrei <andreigridin918@gmail.com>
2023-05-23 20:06:38 +0500

    Add Examle 005

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine()!;
+if(username.ToLower() == "маша")
+{
+    Console.Write("Ура, это же ");
+    Console.WriteLine(username);
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
