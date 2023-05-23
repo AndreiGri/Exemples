2691c5aaece09dd8f7d39bb2cf0857537c9a6252
Gridin_Andrei <andreigridin918@gmail.com>
2023-05-23 20:07:25 +0500

    Add Example 006

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 1;
+int b = 5;
+int c = 8;
+int d = 6;
+int e = 3;
+int max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+Console.Write("Наибольшее число ");
+Console.WriteLine(max);
